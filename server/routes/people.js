f5379f1d7c269219340f0c99fd12a53620fb4b25
twinklefingers <B1-66-ER@Rays-MacBook-Pro.local>
2016-08-12 12:52:03 -0500

    Change styling and get rid of error handling for bad name post.

--- server/routes/people.js
+++ server/routes/people.js
@@ -10,11 +10,11 @@ function Peep(name, bio, url) {
         this.likes = 0
 };
 
-var kim = new Peep('Kim', 'Kim is gone at Summer Set', '../assets/images/Kim.jpg');
-var keri = new Peep('Keri', 'Keri has minions! (children)', '../assets/images/Keri.jpg');
-var joe = new Peep('Joe', 'Joe likes coffee', '../assets/images/Joe.jpg');
-var harry = new Peep('Harry', "Harry won't shut up about Fuzzers", '../assets/images/Harry.jpg');
-var ray = new Peep('Ray', 'Ray has colorful hair', '../assets/images/Ray.jpg');
+var kim = new Peep('Kim', 'Kim is gone at Summer Set.', '../assets/images/Kim.jpg');
+var keri = new Peep('Keri', 'Keri has minions!', '../assets/images/Keri.jpg');
+var joe = new Peep('Joe', 'Give Joe coffee!', '../assets/images/Joe.jpg');
+var harry = new Peep('Harry', "Harry won't shut up.", '../assets/images/Harry.jpg');
+var ray = new Peep('Ray', 'Ray has colorful hair.', '../assets/images/Ray.jpg');
 
 peeps.push(kim, keri, joe, harry, ray);
 
@@ -22,30 +22,43 @@ router.get('/', function(req, res) {
     res.send(peeps);
 });
 
-router.post('/kim', function(req, res) {
-    peeps[0].likes++;
-    console.log("kim: ", peeps[0].likes);
-    res.sendStatus(201);
+router.post("/:name", function(req, res) {
+    var name = req.params.name;
+    peeps.forEach(function(people, i) {
+        if (name == people.name) {
+            peeps[i].likes++;
+            res.sendStatus(201);
+            return;
+        }
+    })
 });
-router.post('/keri', function(req, res) {
-    peeps[1].likes++;
-    console.log("keri: ", peeps[1].likes);
-    res.sendStatus(201);
-});
-router.post('/joe', function(req, res) {
-    peeps[2].likes++;
-    console.log("joe: ", peeps[2].likes);
-    res.sendStatus(201);
-});
-router.post('/harry', function(req, res) {
-    peeps[3].likes++;
-    console.log("harry: ", peeps[3].likes);
-    res.sendStatus(201);
-});
-router.post('/ray', function(req, res) {
-    peeps[4].likes++;
-    console.log("ray: ", peeps[4].likes);
-    res.sendStatus(201);
-});
+
+/* Here you go kris. */
+
+// router.post('/kim', function(req, res) {
+//     peeps[0].likes++;
+//     console.log("kim: ", peeps[0].likes);
+//     res.sendStatus(201);
+// });
+// router.post('/keri', function(req, res) {
+//     peeps[1].likes++;
+//     console.log("keri: ", peeps[1].likes);
+//     res.sendStatus(201);
+// });
+// router.post('/joe', function(req, res) {
+//     peeps[2].likes++;
+//     console.log("joe: ", peeps[2].likes);
+//     res.sendStatus(201);
+// });
+// router.post('/harry', function(req, res) {
+//     peeps[3].likes++;
+//     console.log("harry: ", peeps[3].likes);
+//     res.sendStatus(201);
+// });
+// router.post('/ray', function(req, res) {
+//     peeps[4].likes++;
+//     console.log("ray: ", peeps[4].likes);
+//     res.sendStatus(201);
+// });
 
 module.exports = router;
